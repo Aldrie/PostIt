c6488e4303529f6107dde2e4075c1bed6a716e13
Aldrie <aldriejapa@outlook.com>
2020-03-14 18:28:05 -0300

    (frontend): Configures routes

--- postit-frontend/src/routes/index.js
+++ postit-frontend/src/routes/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
 import AuthPage from 'pages/Auth';
-import LoginContainer from 'containers/LoginContainer';
+import LoginContainer from 'containers/Auth/LoginContainer';
+import RegisterContainer from 'containers/Auth/RegisterContainer';
+import PresentationPage from 'pages/Presentation';
 
 const Routes = () => {
 	return(
 		<BrowserRouter>
-			<Route path="/">
+			<Route path="/" exact component={PresentationPage}/>
+
+			<Route path="/auth">
 				<AuthPage>
-					<LoginContainer />
+					<Switch>
+						<Route path="/auth/login" component={LoginContainer} exact/>
+						<Route path="/auth/register" component={RegisterContainer} exact/>
+					</Switch>
 				</AuthPage>
 			</Route>
 		</BrowserRouter>
