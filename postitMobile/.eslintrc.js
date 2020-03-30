f52ba130de600052bf94cd9ec458b6ef642a6293
Aldrie <aldriejapa@outlook.com>
2020-03-30 19:16:59 -0300

    Starts mobile app

---
+++ postitMobile/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+  "import/resolver": {
+    "babel-plugin-root-import":  {
+      "rootPathPrefix": "~",
+      "rootPathSuffix": "src"
+    }
+  }
+};
---
+++ postitMobile/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>AAA</Text>
+  </View>
+);
+export default App;
